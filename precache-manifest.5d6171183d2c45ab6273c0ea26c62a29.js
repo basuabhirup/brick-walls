34bc35854ae0914b43832b0f8b4d811ee00a1778
basuabhirup <basu.abhirup@yahoo.com>
2020-08-26 14:31:11 +0530

    Updates

---
+++ precache-manifest.5d6171183d2c45ab6273c0ea26c62a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f52cb6047510553f0bf53fd19ac26b0",
+    "url": "/brick-wall/index.html"
+  },
+  {
+    "revision": "31d40f8ecf4230e75dfe",
+    "url": "/brick-wall/static/css/main.3df0ca61.chunk.css"
+  },
+  {
+    "revision": "80324c30f4f3d684b221",
+    "url": "/brick-wall/static/js/2.54423ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brick-wall/static/js/2.54423ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d40f8ecf4230e75dfe",
+    "url": "/brick-wall/static/js/main.03988952.chunk.js"
+  },
+  {
+    "revision": "7880c098d1e8474df8f7",
+    "url": "/brick-wall/static/js/runtime-main.2dd8af17.js"
+  }
+]);
